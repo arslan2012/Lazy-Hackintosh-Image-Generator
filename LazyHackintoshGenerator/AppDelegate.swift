aa7c5f866e97374a8d261ccde6e282a5679378a3
Arslan Ablikim <arslan.ablikim0@gmail.com>
2016-02-05 11:07:55 +0800

    Initial Commit

---
+++ LazyHackintoshGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LazyHackintoshGenerator
+//
+//  Created by ئ‍ارسلان ئابلىكىم on 2/5/16.
+//  Copyright © 2016 PCBETA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LazyHackintoshGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LazyHackintoshGenerator
+//
+//  Created by ئ‍ارسلان ئابلىكىم on 2/5/16.
+//  Copyright © 2016 PCBETA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
