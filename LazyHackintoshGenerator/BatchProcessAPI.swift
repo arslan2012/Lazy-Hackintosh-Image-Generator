bee0757a6ecd10922958dc1f623d3503b0786f42
Arslan Ablikim <arslan.ablikim0@gmail.com>
2016-06-13 14:06:41 +0800

    fixed wrong usage of /bin/sh

--- LazyHackintoshGenerator/BatchProcessAPI.swift
+++ LazyHackintoshGenerator/BatchProcessAPI.swift
@@ -150,7 +150,7 @@ class BatchProcessAPI{
 			self.shellCommand("/bin/mkdir",arg: [lazypath+"/System/Library/Kernels"], label: "#CREATEKERNELSF#", progress: 1)
 			////////////////////////////patching processes////////////////////////
 			if MBRPatchState {
-				self.privilegedShellCommand("/bin/sh",arg: ["/usr/bin/perl","-pi","-e","'s|\\x48\\x8B\\x78\\x28\\x48\\x85\\xFF\\x74\\x5F\\x48\\x8B\\x85|\\x48\\x8B\\x78\\x28\\x48\\x85\\xFF\\xEB\\x5F\\x48\\x8B\\x85|g'",lazypath+"/System/Library/PrivateFrameworks/OSInstaller.framework/Versions/A/OSInstaller"], label: "#PATCH01#",progress: 1)
+				self.privilegedShellCommand("/bin/sh",arg: ["-c","perl -pi -e 's|\\x48\\x8B\\x78\\x28\\x48\\x85\\xFF\\x74\\x5F\\x48\\x8B\\x85|\\x48\\x8B\\x78\\x28\\x48\\x85\\xFF\\xEB\\x5F\\x48\\x8B\\x85|g' "+lazypath+"/System/Library/PrivateFrameworks/OSInstaller.framework/Versions/A/OSInstaller"], label: "#PATCH01#",progress: 1)
 				self.privilegedShellCommand("/usr/bin/codesign",arg: ["-f","-s","-",lazypath+"/System/Library/PrivateFrameworks/OSInstaller.framework/Versions/A/OSInstaller"], label: "#PATCH01#",progress: 1)
 				
 				self.shellCommand("/bin/mkdir",arg: ["/tmp/com.pcbeta.lazy/osinstallmpkg"], label: "#PATCH02#", progress: 0)
@@ -174,9 +174,9 @@ class BatchProcessAPI{
 			}
 			if XCPMPatchState {
 				self.shellCommand("/bin/cp",arg: [kernelDroppedFilePath,lazypath+"/System/Library/Kernels"], label: "#COPYKERNELF#", progress: 1)
-				self.shellCommand("/bin/sh",arg: ["/usr/bin/perl","-pi","-e","'s|\\xe2\\x00\\x00\\x00\\x02\\x00\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g'",lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 0)
-				self.shellCommand("/bin/sh",arg: ["/usr/bin/perl","-pi","-e","'s|\\xe2\\x00\\x00\\x00\\x4c\\x00\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g'",lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 0)
-				self.shellCommand("/bin/sh",arg: ["/usr/bin/perl","-pi","-e","'s|\\xe2\\x00\\x00\\x00\\x90\\x01\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g'",lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 1)
+				self.shellCommand("/bin/sh",arg: ["-c","perl -pi -e 's|\\xe2\\x00\\x00\\x00\\x02\\x00\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g' "+lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 0)
+				self.shellCommand("/bin/sh",arg: ["-c","perl -pi -e 's|\\xe2\\x00\\x00\\x00\\x4c\\x00\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g' "+lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 0)
+				self.shellCommand("/bin/sh",arg: ["-c","perl -pi -e 's|\\xe2\\x00\\x00\\x00\\x90\\x01\\x00\\x00|\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00|g' "+lazypath+"/System/Library/Kernels/kernel"], label: "#XCPMPATCH#",progress: 1)
 			}else {
 				self.shellCommand("/bin/cp",arg: [kernelDroppedFilePath,lazypath+"/System/Library/Kernels"], label: "#COPYKERNELF#", progress: 2)
 			}
